550c56fa8f4b0056826d1283ec75e1f68483acc7
dwebsites <dondim@protonmail.com>
2021-12-10 16:19:19 +0100

    Add local postgers connection

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -1,4 +1,5 @@
 import os
+from app import app
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,4 +11,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/fyyur_dev'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
