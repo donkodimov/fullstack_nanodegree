dab9d0fc1d5c1324d893833bf5e222addeaa96ae
dwebsites <dondim@protonmail.com>
2021-12-20 21:09:33 +0100

    refactor: Use ShowForm() for request data

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -376,16 +376,14 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-  # called to create new shows in the db, upon submitting new show listing form
-  # TODO: insert form data as a new Show record in the db, instead
-  form_input = request.form
-  print(form_input) 
+
+  form = ShowForm(request.form)
   try:
-      show = Show(**form_input)
+      show = Show(artist_id = form.artist_id.data,
+                  venue_id = form.venue_id.data,
+                  start_time =form.start_time.data)
       db.session.add(show)
-      db.session.commit()
-      flash('Show was successfully listed!')
-      return render_template('pages/home.html')
+      db.session.commit() 
   except:
       db.session.rollback()
       flash('An error occurred. Show could not be listed.')
@@ -393,6 +391,8 @@ def create_show_submission():
       return render_template('errors/500.html')
   finally:
       db.session.close()
+      flash('Show was successfully listed!')
+      return render_template('pages/home.html')
   
   # on successful db insert, flash success
   # TODO: on unsuccessful db insert, flash an error instead.
